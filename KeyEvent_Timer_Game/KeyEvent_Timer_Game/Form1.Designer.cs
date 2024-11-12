ef7a3799d0c1f2e33e1ddede3642cbaee4914af1
SebastianWojczyk <62283873+SebastianWojczyk@users.noreply.github.com>
2024-11-12 11:35:14 +0100

    Add files via upload

---
+++ KeyEvent_Timer_Game/KeyEvent_Timer_Game/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿
+namespace KeyEvent_Timer_Game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.pictureBoxGame = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxGame)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer
+            // 
+            this.timer.Enabled = true;
+            this.timer.Interval = 25;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // pictureBoxGame
+            // 
+            this.pictureBoxGame.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBoxGame.Location = new System.Drawing.Point(0, 0);
+            this.pictureBoxGame.Name = "pictureBoxGame";
+            this.pictureBoxGame.Size = new System.Drawing.Size(800, 450);
+            this.pictureBoxGame.TabIndex = 0;
+            this.pictureBoxGame.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBoxGame);
+            this.KeyPreview = true;
+            this.MaximumSize = new System.Drawing.Size(816, 489);
+            this.MinimumSize = new System.Drawing.Size(816, 489);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxGame)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.PictureBox pictureBoxGame;
+    }
+}
+
---
+++ KeyEvent_Timer_Game/KeyEvent_Timer_Game/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KeyEvent_Timer_Game
+{
+    public partial class Form1 : Form
+    {
+        Graphics graph;
+
+        Rectangle player;
+        Color plyerColor = Color.Brown;
+        Keys playerDirection = Keys.None;
+
+        Rectangle ball;
+        Color ballColor = Color.Green;
+        Point ballSpeed;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            //From documentation:
+            //Gets or sets a value indicating whether the form will receive key events
+            //before the event is passed to the control that has focus.
+            this.KeyPreview = true;
+
+            pictureBoxGame.Image = new Bitmap(pictureBoxGame.Width, pictureBoxGame.Height);
+            graph = Graphics.FromImage(pictureBoxGame.Image);
+
+            player = new Rectangle(10, 10, 30, 100);
+            ball = new Rectangle(pictureBoxGame.Width / 2, pictureBoxGame.Height / 2, 30, 30);
+            ballSpeed = new Point(10, 5);
+
+            drawGame();
+        }
+
+        private void drawGame()
+        {
+            graph.Clear(Color.Yellow);
+
+            graph.FillEllipse(new SolidBrush(ballColor), ball);
+
+            graph.FillRectangle(new SolidBrush(plyerColor), player);
+
+
+            //pause
+            if (!timer.Enabled)
+            {
+                graph.DrawString("PAUSE",
+                                 new Font(Font.FontFamily, 100),
+                                 new SolidBrush(Color.Red),
+                                 new Point(10, 10));
+            }
+
+            pictureBoxGame.Refresh();
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            if (playerDirection == Keys.Up && player.Y > 10)
+            {
+                player.Y -= 10;
+            }
+            else if (playerDirection == Keys.Down && player.Y + player.Height + 10 < pictureBoxGame.Height)
+            {
+                player.Y += 10;
+            }
+            //right border or player
+            if (ball.X + ball.Width > pictureBoxGame.Width ||
+               player.IntersectsWith(ball))
+            {
+                ballSpeed.X = -ballSpeed.X;
+            }
+            //top/bottom border
+            if (ball.Y < 0 ||
+                ball.Y + ball.Height > pictureBoxGame.Height)
+            {
+                ballSpeed.Y = -ballSpeed.Y;
+            }
+
+            //left border - end game
+            if (ball.X < 0)
+            {
+                timer.Stop();
+                MessageBox.Show("End game!");
+                Close();
+            }
+
+            ball.X += ballSpeed.X;
+            ball.Y += ballSpeed.Y;
+
+            drawGame();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up || e.KeyCode == Keys.Down)
+            {
+                playerDirection = e.KeyCode;
+            }
+            else if (e.KeyCode == Keys.Space)
+            {
+                timer.Enabled = !timer.Enabled;
+                drawGame();
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up || e.KeyCode == Keys.Down)
+            {
+                playerDirection = Keys.None;
+            }
+        }
+    }
+}
